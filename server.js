8ffaffcc04ea3dbba3f480e62b0df7c40f02c794
Wen Ow <ow.wen2022@gmail.com>
2025-01-30 21:04:00 -0500

    coded CRUD operators

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override"); 
+const morgan = require("morgan");
+
 
 const app = express();
 
@@ -17,6 +20,8 @@ mongoose.connection.on("connected", () => {
 const Fruit = require("./models/fruit.js");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 // Routes
 
@@ -25,14 +30,18 @@ app.use(express.urlencoded({ extended: false }));
 //     res.send("hello, friend!");
 // });
 
+// Home / Landing
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 // I.N.D.U.C.E.S. - Nemonic device
 // I. = Index
 // Get / Root Route
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render("fruits/index.ejs", { fruits: allFruits });
 });
-
 
 // N. = New
 // GET /fruits/new
@@ -40,12 +49,27 @@ app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
 
-
 // D. = Delete
-
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
 
 // U. = Update
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
 
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 // C. = Create
 // POST /fruits
@@ -56,17 +80,25 @@ app.post("/fruits", async (req, res) => {
     req.body.isReadyToEat = false;
   }
   await Fruit.create(req.body);
-  
-  res.redirect("/fruits/new");
+  res.redirect("/fruits"); // redirect to index fruits
 });
 
 // E. = Edit
-
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
 
 // S. = Show
-
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
+});
 
 // Port - One of 3 ways to use the port variable
 app.listen(3000, () => {
   console.log("Listening on port, 3000");
 });
+
